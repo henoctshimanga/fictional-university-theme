ee9ec7e496e3a2a0b374b89f332449c64a686b80
henoctshimanga <henoctshimanga79@gmail.com>
2025-02-13 19:49:46 -0500

    added metabox to the single blog page

--- single.php
+++ single.php
@@ -16,7 +16,11 @@
     </div>
 
     <div class="container container--narrow page-section">
-        metabox will go header_register_callback
+                <div class="metabox metabox--position-up metabox--with-home-link">
+                    <p>
+                    <a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home</a> <span class="metabox__main"><?php the_title(); ?> </span>
+                    </p>
+                </div>
 
         <div class="generic-content">
             <?php the_content(); ?>
