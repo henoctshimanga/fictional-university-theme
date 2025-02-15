e944dd658130c7fba2e7f3eb4a6e2f91bf581f59
henoctshimanga <henoctshimanga79@gmail.com>
2025-02-14 22:18:32 -0500

    Archive title added

--- single.php
+++ single.php
@@ -18,7 +18,8 @@
     <div class="container container--narrow page-section">
                 <div class="metabox metabox--position-up metabox--with-home-link">
                     <p>
-                    <a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home</a> <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('M, d Y'); ?> in <?php echo get_the_category_list(', '); ?></span>
+                    <a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home</a> 
+                    <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('M, d Y'); ?> in <?php echo get_the_category_list(', '); ?></span>
                     </p>
                 </div>
 
