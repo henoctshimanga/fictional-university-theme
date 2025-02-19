a243e0b9d7f9f636776ed2f2879b0784aec8eaa3
henoctshimanga <henoctshimanga79@gmail.com>
2025-02-19 13:22:46 -0500

    added arguments to Query and sorting event by the nearest upcoming event to the laster one

--- front-page.php
+++ front-page.php
@@ -17,8 +17,11 @@
           
           <?php
             $homepageEvents = new WP_Query(array(
-              'posts_per_page' => 2,
-              'post_type' => 'event'
+              'posts_per_page' => -1,
+              'post_type' => 'event',
+              'meta_key' => 'event_date',
+              'orderby' => 'meta_value_num',
+              'order' => 'ASC' //Ascending order
             ));
 
             while($homepageEvents->have_posts()) {
