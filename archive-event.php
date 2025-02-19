538544d94ad9569066850527cd9731b0535a502d
henoctshimanga <henoctshimanga79@gmail.com>
2025-02-19 11:35:46 -0500

    fixed layout to be dynamic and direct to related pages

--- archive-event.php
+++ archive-event.php
@@ -6,7 +6,7 @@
       <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title">All Events</h1>
         <div class="page-banner__intro">
-          <p><?php the_archive_description(); ?></p>
+          <p>See What is going on in our world</p>
         </div>
       </div>
     </div>
@@ -16,19 +16,16 @@
         while(have_posts()) {
           the_post(); ?>
 
-        <div class="post-item">
-          <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-
-          <div class="metabox">
-            <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('M, d Y'); ?> in <?php echo get_the_category_list(', '); ?></p>
-          </div>
-
-            <div class="generic-content">
-              <?php the_excerpt(); ?>
-              <p><a class="btn--blue btn" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
-            </div>
-
-        </div>
+              <div class="event-summary">
+                <a class="event-summary__date t-center" href="#">
+                  <span class="event-summary__month"><?php the_time('M');?></span>
+                  <span class="event-summary__day"><?php the_time('d');?></span>
+                </a>
+                <div class="event-summary__content">
+                  <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                  <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                </div>
+              </div>
 
        <?php }
        echo paginate_links();
